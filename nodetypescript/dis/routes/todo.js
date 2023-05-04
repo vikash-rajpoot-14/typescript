ec198d11142093f27d1f7451f2f0572a6587cb4f
vikash rajpoot <vikashraj1490@gmail.com>
2023-05-04 12:35:41 +0530

    typecasting

--- nodetypescript/dis/routes/todo.js
+++ nodetypescript/dis/routes/todo.js
@@ -7,23 +7,27 @@ router.get('/', (req, res, next) => {
     res.status(200).json({ message: "fetched todo successfully", todo: todo });
 });
 router.post('/todo', (req, res, next) => {
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     };
     todo.push(newTodo);
     res.status(201).json({ message: "todo added successfully", todo: newTodo });
 });
 router.put('/todo/:todoId', (req, res, next) => {
-    const Index = todo.findIndex((ele) => ele.id === req.params.todoId);
+    const todoparam = req.params;
+    const body = req.body;
+    const Index = todo.findIndex((ele) => ele.id === todoparam.todoId);
     if (Index >= 0) {
-        todo[Index] = { id: todo[Index].id, text: req.body.text };
+        todo[Index] = { id: todo[Index].id, text: body.text };
         return res.status(200).json({ message: "updated todo succesfully", todo });
     }
     res.status(404).json({ message: "todo not found" });
 });
 router.delete('/todo/:todoId', (req, res, next) => {
-    todo = todo.filter((ele) => ele.id !== req.params.todoId);
+    const todoparam = req.params;
+    todo = todo.filter((ele) => ele.id !== todoparam.todoId);
     res.status(204).json({ message: "todo deleted succesfully", todo: todo });
 });
 exports.default = router;
